74e796e31760064100b533fe00f41ccbc12f4c0d
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-10-11 14:41:25 +0100

    Adding vanilla page with placeholder event

---
+++ index.js
@@ -0,0 +1,6 @@
+function getChatAnswer() {
+    const response = "By checking out Carly's code!";
+    const responseElement = document.getElementById("chat-response");
+
+    responseElement.innerText = response;
+}
