1b5d891da083923798679aa57384e14f48903bdd
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-10-16 10:31:18 +0100

    Wiring up Elasticsearch to get top result

--- server/server.js
+++ server/server.js
@@ -1,20 +1,40 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const { getTopDocumentForQuestion } = require("./util/elasticsearch");
+
+const app = express();
 app.use(cors());
 
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('I am the H.A.L 9000. You may call me Hal.')
-})
+app.get("/", (req, res) => {
+  res.send("I am the H.A.L 9000. You may call me Hal.");
+});
 
-app.get('/chat', (req, res) => {
-    const question = req.query.question;
-    res.send(`You asked me ${question}. I can't let you do that Dave.`)
-  })
+app.get("/chat", async (req, res) => {
+  const question = req.query.question;
+  let solution;
+
+  if (question) {
+    const decodedQuestion = decodeURIComponent(question);
+
+    try {
+      const response = await getTopDocumentForQuestion(question);
+      const document = response.hits?.hits[0];
+
+      if (document) {
+        solution = document._source.solution.text;
+      }
+    } catch(e) {
+      console.log(e);
+      solution = "I can't let you do that Dave."
+    }
+  }
+
+  res.send(solution);
+});
 
 app.listen(port, () => {
-  console.log(`Hal is listening on port ${port}`)
-})
+  console.log(`Hal is listening on port ${port}`);
+});
---
+++ server/util/elasticsearch.js
@@ -0,0 +1,34 @@
+const elasticsearch = require("@elastic/elasticsearch");
+
+const cloudID = process.env.ELASTIC_CLOUD_ID;
+const apiKey = process.env.ELASTIC_API_KEY;
+const index = "bytes-discuss-dense-search";
+
+const client = new elasticsearch.Client({
+  cloud: { id: cloudID },
+  auth: { apiKey: apiKey },
+});
+
+async function getTopDocumentForQuestion(question) {
+  if (!client) {
+    throw new Error("Unable to connect to Elasticsearch")
+  }
+    return client.search({
+        index: index,
+        fields: ["solution.text"],
+        knn: {
+            field: "title_vector.predicted_value",
+            k: 1,
+            num_candidates: 100,
+            query_vector_builder: {
+              text_embedding: { 
+                model_id: "sentence-transformers__all-mpnet-base-v2", 
+                model_text: question
+              }
+            }
+          }
+        
+    });
+}
+
+module.exports = { getTopDocumentForQuestion }
