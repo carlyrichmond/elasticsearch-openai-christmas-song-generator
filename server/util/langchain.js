d0486916c162fa7a5853726742129d4dab34c863
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-11-23 17:27:29 +0000

    Changing to use prompts

--- server/util/langchain.js
+++ server/util/langchain.js
@@ -1,14 +1,34 @@
-const openai = require("langchain/llms/openai");
+const openai = require("langchain/chat_models/openai");
+const prompts = require("langchain/prompts");
 
 const apiKey = process.env.OPENAI_API_KEY;
-const llm = new openai.OpenAI({
+const chatModel = new openai.ChatOpenAI({
   openAIApiKey: apiKey,
   temperature: 0.9
 });
 
   async function getChristmasSongFromLyrics(subject, adjective, food, gift, lyrics) {
-    const prompt = `Write a Christmas song about ${subject}, describing Christmas time as ${adjective} including reference to eating ${food} and receiving a ${gift} in the style of the following song lyrics: ${lyrics}`;
-    return llm.predict(prompt);
+    //const prompt = `Write a Christmas song about ${subject}, describing Christmas time as ${adjective} including reference to eating ${food} and receiving a ${gift} in the style of the following song lyrics: ${lyrics}`;
+
+    const chatPrompts = prompts.ChatPromptTemplate.fromMessages([
+      ["system", "You are a Christmas song generator that takes user input and generates a song in the style of the provided song"],
+      ["human", "Write a {adjective} Christmas song about {subject}"],
+      ["human", "Include at least one reference to eating {food} and receiving {gift}"],
+      ["human", "Write the song in the style of these lyrics: {lyrics}"],
+    ]);
+
+    const formattedChatPrompts = await chatPrompts.formatMessages({
+      subject: subject,
+      adjective: adjective,
+      food: food,
+      gift: gift,
+      lyrics: lyrics
+    });
+
+    const messages = await chatModel.predictMessages(formattedChatPrompts);
+
+    return messages.content;
+
   }
 
   module.exports = { getChristmasSongFromLyrics: getChristmasSongFromLyrics }
