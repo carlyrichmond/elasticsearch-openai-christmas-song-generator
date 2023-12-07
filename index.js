42c9dc98e4f34bc1309a6ae9362960400ad8dd9f
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-12-07 14:51:24 +0000

    Adding additional params back in

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 async function getSong() {
   const subject = document.getElementById("subject-input").value;
   const songTitle = document.getElementById("song-title-input").value;
+  const adjective = document.getElementById("adjective-input").value;
+  const food = document.getElementById("food-input").value;
+  const gift = document.getElementById("gift-input").value;
   
   // Add loading message
   const responseElement = document.getElementById("song-response");
@@ -10,10 +13,12 @@ async function getSong() {
 
   try {
     const encodedSubject = encodeURIComponent(subject);
-
     const encodedSongTitle = encodeURIComponent(songTitle);
+    const encodedAdjective = encodeURIComponent(adjective);
+    const encodedFood = encodeURIComponent(food);
+    const encodedGift = encodeURIComponent(gift);
 
-    const response = await fetch(`http://localhost:3000/generate?subject=${encodedSubject}&title=${encodedSongTitle}`);
+    const response = await fetch(`http://localhost:3000/generate?subject=${encodedSubject}&title=${encodedSongTitle}&adjective=${encodedAdjective}&food=${encodedFood}&gift=${encodedGift}`);
 
     answer = await response.text();
 
